ff9a2d1642cb144b2929a522df0384f88e49462e
Gustavo Martins <martinshenriquegustavo@gmail.com>
2023-11-28 18:20:06 -0300

    Cut Sprite back and working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,9 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     [SerializeField] private List<PieceClass> pieces = new List<PieceClass>();
     [SerializeField] private List<Slot> slots = new List<Slot>();
-    [SerializeField] private GameObject pieceObjectReference, slot;
+    [SerializeField] private GameObject slot;
     public GameObject fatherOfPieces;
-    [SerializeField] private Image[] panelObjects;
-    [SerializeField] private Image panelGrid;
+    [SerializeField] private GameObject panelGrid;
     [Space(2)]
     [Header("Input")]
     public InputManager inputManager;
@@ -19,9 +18,7 @@ public class GameManager : MonoBehaviour
 
     [Space(3)]
     [Header("Sprite")]
-    public Piece[] scriptablePiece;
     [SerializeField] private Piece piecePart;
-    [SerializeField] private Sprite panelGridImage;
 
 
     [Header("PanelWin")]
@@ -35,15 +32,13 @@ public class GameManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
-        }
-
-        PanelAdjustment();
-        PlacePiecesAndSlots();
+        }  
     }
 
     private void Start()
     {
         inputManager.OnPieceChanged += PieceManager_OnPieceChanged;
+        PlacePiecesAndSlots();
     }
     private void PieceManager_OnPieceChanged(object sender, bool e)
     {
@@ -63,28 +58,19 @@ public class GameManager : MonoBehaviour
             panelWin.SetActive(true);
         }
     }
-    private void PanelAdjustment()
-    {
-        piecePart = scriptablePiece[Random.Range(0, scriptablePiece.Length)];
-        GameObject panel = Instantiate(panelObjects[(int)piecePart.type].gameObject, fatherOfPieces.transform.parent);
-        panel.transform.SetAsFirstSibling();
-        panelGrid = panel.GetComponent<Image>();
-        panelGridImage = piecePart.panelGridImage;
-        panelGrid.sprite = panelGridImage;
-    }
 
     private void PlacePiecesAndSlots()
     {
-        for (int x = 0; x < piecePart.piecePart.Length - 1; x++)
+        for (int x = 0; x < piecePart.sprites.Count; x++)
         {
-            GameObject piece = Instantiate(pieceObjectReference, fatherOfPieces.transform);
-            PieceClass pieceClass = piece.GetComponent<PieceClass>();
+            PieceClass pieceClass = piecePart.sprites[x].GetComponent<PieceClass>();
             pieces.Add(pieceClass);
             pieceClass.SetId(x);
-            pieceClass.SetSprite(piecePart.piecePart[x]);
+            pieceClass.SetSprite(piecePart.sprites[x].sprite);
             PositionAndRotation(pieceClass);
             GameObject gameSlot = Instantiate(this.slot, panelGrid.transform);
             Slot slot = gameSlot.GetComponent<Slot>();
+            slot.GetComponent<Image>().rectTransform.sizeDelta = piecePart.sprites[x].rectTransform.sizeDelta;
             slot.id = x;
             slots.Add(slot);
         }
